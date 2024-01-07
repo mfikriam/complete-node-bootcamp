71e1e1f10a11828c16d00d6bc65fd16941e2210b
mfikriam <yapkun17@gmail.com>
2024-01-07 22:01:52 +0800

    143. Implementing Rate Limiting

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,10 +9,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// ? MIDDLEWARES
+// ? GLOBAL MIDDLEWARES
+
+// ? Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// ? Limit requests from same API
+const limiter = rateLimit({
+  max: 100, // ? 100 request per
+  windowMs: 60 * 60 * 1000, // ? one hour
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -25,7 +25,7 @@ const createSendToken = (user, statusCode, res) => {
 
   res.cookie('jwt', token, cookieOptions);
 
-  // ? remove password from the output
+  // ? Remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
