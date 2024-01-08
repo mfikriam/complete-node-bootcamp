65b2a1a886ef07e8b39b6161d60ae25224fae33d
mfikriam <yapkun17@gmail.com>
2024-01-09 00:24:09 +0800

    165. Adding Missing Authentication and Authorization

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -6,19 +6,10 @@ const reviewRouter = require('./../routes/reviewRoutes');
 const router = express.Router();
 
 // router.param('id', tourController.checkId);
-// .post(tourController.checkBody, tourController.createTour);
 
 // * POST /tours/p12igt4ss/reviews
 // * GET /tours/p12igt4ss/reviews
 // * GET /tours/p12igt4ss/reviews/s9s8f77sa
-
-// router
-//   .route('/:tourId/reviews')
-//   .post(
-//     authController.protect,
-//     authController.restrictTo('user'),
-//     reviewController.createReview,
-//   );
 
 router.use('/:tourId/reviews', reviewRouter);
 
@@ -27,17 +18,31 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan,
+  );
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour,
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour,
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
