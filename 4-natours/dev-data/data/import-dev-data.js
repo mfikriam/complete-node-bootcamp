85df518189fe881054da0d37227a9b7c5224ddd0
mfikriam <yapkun17@gmail.com>
2024-01-08 10:15:03 +0800

    150. Modelling Locations (Geospatial Data)

--- 4-natours/dev-data/data/import-dev-data.js
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -32,7 +32,8 @@ mongoose
 
 // ? Read JSON File
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  // fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+  fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'),
 );
 
 // ? Import Data Into DB
