f314e6870e6faf9b955791f0001dab231ed2c2bc
mfikriam <yapkun17@gmail.com>
2024-01-09 23:24:45 +0800

    190. Logging in Users with Our API - Part 2

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,9 +1,8 @@
 /* eslint-disable */
 
 const login = async (email, password) => {
-  console.log(email, password);
   try {
-    const result = await axios({
+    const res = await axios({
       method: 'POST',
       url: 'http://localhost:3000/api/v1/users/login',
       data: {
@@ -12,9 +11,14 @@ const login = async (email, password) => {
       },
     });
 
-    console.log(result);
+    if (res.data.status === 'success') {
+      alert('Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
   } catch (err) {
-    console.log(err.response.data);
+    alert(err.response.data.message);
   }
 };
 
