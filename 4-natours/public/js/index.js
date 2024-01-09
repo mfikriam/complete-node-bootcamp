e5d7eeb720223c38688763d3e159db9946e342ba
mfikriam <yapkun17@gmail.com>
2024-01-10 00:38:27 +0800

    192. Logging out Users

--- 4-natours/public/js/index.js
+++ 4-natours/public/js/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login } from './login';
+import { login, logout } from './login';
 
 //? DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 //? DELEGATIONS
 if (mapBox) {
@@ -14,10 +15,14 @@ if (mapBox) {
 }
 
 if (loginForm) {
-  document.querySelector('.form').addEventListener('submit', (e) => {
+  loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     login(email, password);
   });
+}
+
+if (logOutBtn) {
+  logOutBtn.addEventListener('click', logout);
 }
