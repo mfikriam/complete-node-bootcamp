a4da9eb70b3e2146e0382dcae89665dfcdbd07a5
mfikriam <yapkun17@gmail.com>
2024-01-11 11:02:19 +0800

    203. Adding Image Uploads to Form

--- 4-natours/public/js/index.js
+++ 4-natours/public/js/index.js
@@ -10,6 +10,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const fileInput = document.querySelector('.form__upload');
 
 //? DELEGATIONS
 if (mapBox) {
@@ -33,9 +34,13 @@ if (logOutBtn) {
 if (userDataForm) {
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings({ name, email }, 'data');
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    // form.append('photo', document.getElementById('photo').files[0]);
+
+    // console.log(form);
+    updateSettings(form, 'data');
   });
 }
 
@@ -58,3 +63,21 @@ if (userPasswordForm) {
     document.getElementById('password-confirm').value = '';
   });
 }
+
+if (fileInput)
+  fileInput.addEventListener('change', async (e) => {
+    const form = new FormData();
+    form.append('photo', document.getElementById('photo').files[0]);
+
+    // Take care of the type attribute being photo
+    const newImage = await updateSettings(form, 'photo');
+
+    if (newImage) {
+      document
+        .querySelector('.nav__user-img')
+        .setAttribute('src', `/img/users/${newImage}`);
+      document
+        .querySelector('.form__user-photo')
+        .setAttribute('src', `/img/users/${newImage}`);
+    }
+  });
